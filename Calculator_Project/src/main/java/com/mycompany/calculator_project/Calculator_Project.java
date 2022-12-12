361e0f4bd219c3eb7c5533993663eaf49baffd6d
Mahmoud Reda <113782853+mahmoudreda12@users.noreply.github.com>
2022-12-12 23:30:04 +0200

    new line

--- Calculator_Project/src/main/java/com/mycompany/calculator_project/Calculator_Project.java
+++ Calculator_Project/src/main/java/com/mycompany/calculator_project/Calculator_Project.java
@@ -1,8 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
-
 package com.mycompany.calculator_project;
 
 /**
@@ -13,5 +8,6 @@ public class Calculator_Project {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Forward");
     }
 }
